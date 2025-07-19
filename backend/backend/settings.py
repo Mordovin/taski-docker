1eaaab86f761d60f059c74b42b3a9b929efd10a4
Danil Mordovin <Mordovin.D@yandex.ru>
2025-07-19 18:25:38 +0200

    Add workflow with PEP8 linting + django tests + docker

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/app/collected_static'
+STATIC_ROOT = os.environ.get('STATIC_ROOT', BASE_DIR / 'collected_static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

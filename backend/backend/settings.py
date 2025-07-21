2e239949c93aa991deeb15e957c38dcdd2787996
Danil Mordovin <Mordovin.D@yandex.ru>
2025-07-21 18:41:29 +0200

    Add workflow with PEP8 linting + django tests + docker

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -105,5 +105,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
-    'http://https://mordovind.zapto.org:3000'
+    'http://https://mordovind.zapto.org'
 ]
